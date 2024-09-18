40be0917650577446beac9ceb919d24eb9622bcf
JustinasPakalnis <justinas.p@inbox.lt>
2024-09-18 22:03:53 +0300

    dabar jau tikrai

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalContext";
 import { SidePanel } from "../side_panel/SidePanel";
 import { TopPanel } from "../top_panel/TopPanel";
+import { useState } from "react";
 export function Main() {
   const { column, row, addRow, addColumn, remoweColumn, remoweRow } =
     useContext(GlobalContext);
@@ -9,16 +10,23 @@ export function Main() {
   const columnArray = Array(column).fill();
   console.log(rowArray);
   console.log(columnArray);
+  const [blockClass, setBlockClass] = useState("block");
 
   return (
     <main>
       <TopPanel></TopPanel>
       <SidePanel></SidePanel>
       <div className="container">
-        {rowArray.map((row, rowIndex) => (
-          <div key={rowIndex} className="block">
-            {columnArray.map((row, colIndex) => (
-              <div key={colIndex} className="block1"></div>
+        {rowArray.map((div, index) => (
+          <div key={index} className={blockClass}>
+            {columnArray.map((div, index) => (
+              <div
+                onClick={() =>
+                  setBlockClass(blockClass === "block" ? "block1" : "block")
+                }
+                key={index}
+                className={blockClass}
+              ></div>
             ))}
           </div>
         ))}
